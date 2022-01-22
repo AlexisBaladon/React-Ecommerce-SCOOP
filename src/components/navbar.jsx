70a9d09ddb71430269f9fc7b8fa0bc5ffec10288
alexis.baladon <alexis.baladon@fing.edu.uy>
2022-01-21 23:45:59 -0300

    hamburguesa feliz

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,27 @@
-import {Navbar as BTNavBar, Nav, Navbar, Container} from 'react-bootstrap';
+import {Navbar as BTNavBar, Nav, Container} from 'react-bootstrap';
 import './navbar.css';
+import logo from './icon.png'
 
 const NavBar = () => {
   return <>
     <BTNavBar id="navegador" bg="" expand="lg" variant="light">
-        <Container fluid className="">
-            <BTNavBar.Brand href="#">
-                <img className="logo" src='icon.png' width="100px" height="100px" alt="Scoop" />
-            </BTNavBar.Brand>
-            <BTNavBar.Toggle id="nav-toggle" bg=""  variant="light" aria-controls="navbarScroll" />
-            <BTNavBar.Collapse id="navbarScroll" className="justify-content-end">
-                <Nav
-                    className="nav"
-                    style={{ maxHeight: '100px'}}
-                    navbarScroll
-                >
-                    <Nav.Link href="#action1">Inicio</Nav.Link>
-                    <Nav.Link href="#action2">Productos</Nav.Link>
-                    <Nav.Link href="#action3">Contacto</Nav.Link>
-                </Nav>
-            </BTNavBar.Collapse>
-        </Container>
+      <Container>
+        <BTNavBar.Brand href="#home">
+          <div id="logo-nav" className="logo"/>
+          <img src={logo} width="100px" height="100px" alt="Scoop" />
+        </BTNavBar.Brand>
+        <BTNavBar.Toggle id="nav-toggle" bg=""  variant="light" aria-controls="basic-navbar-nav" />
+        <BTNavBar.Collapse id="navbarScroll" className="justify-content-end">
+          <Nav className="">
+            <Nav.Link href="#Inicio">Inicio</Nav.Link>
+            <Nav.Link href="#Productos">Productos</Nav.Link>
+            <Nav.Link href="#Contacto">Contacto</Nav.Link>
+          </Nav>
+        </BTNavBar.Collapse>
+      </Container>
     </BTNavBar>
   </>;
+
 };
 
 export default NavBar;
