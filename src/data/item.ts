2d7e6b2dedcf25d5a052ab6501d09bcd1262bba1
alexis.baladon <alexis.baladon@fing.edu.uy>
2022-02-06 20:45:12 -0300

    Nuevos productos agregados

--- src/data/item.ts
+++ src/data/item.ts
@@ -12,6 +12,9 @@ const items: Item[] = [
   new Item(5,'Banana'               , 'El preferido de Tarzán'                        ,7  ,imageUrl+'recipientes/vainilla.jpg'       , 6, ItemCategory.Recipiente),
   new Item(6,'Dulce de L.'          , 'No hace falta describirlo...'                  ,16 ,imageUrl+'recipientes/dulce-de-leche.jpg' , 7, ItemCategory.Recipiente),
   new Item(7,'Mora'                 , 'Un sabor que te enamora'                       ,4  ,imageUrl+'recipientes/mora.jpg'           , 9, ItemCategory.Recipiente),
+  new Item(8,'Frutilla'             , 'Tu fruta favorita!'                            ,3  ,imageUrl+'recipientes/frutilla.jpg'          , 9, ItemCategory.Recipiente),
+  new Item(9,'Brownielado'          , 'Brownie y helado... nada más que agregar'      ,10 ,imageUrl+'postres/brownie.jpg'            , 9, ItemCategory.Postre),
+  new Item(10,'Super porción'       , 'Reserva de helado para toda una vida'          ,20 ,imageUrl+'postres/choco-salsa-nuez.jpg'  , 9, ItemCategory.Postre),
 ];
 
 const obtenerHelados = (): Item[] => items;
