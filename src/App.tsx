2c2d7378340e03eb6c9e3d4792d1ebb39a592433
alexis.baladon <alexis.baladon@fing.edu.uy>
2022-02-11 21:58:23 -0300

    cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import ItemChooser from './components/items/detail/itemChooser';
 import Routes from './routes/routes';
-import { obtenerHelados } from './data/item';
-import DtItem from './dataTypes/item';
-import ItemCategory from './dataTypes/itemCategory';
-import { useState } from 'react';
-
 
 function App() {
 
